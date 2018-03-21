ec6e7342aa2838d445cad476dd7f5f40895a676f
walli778 <37522879+walli778@users.noreply.github.com>
2018-03-21 16:08:08 -0300

    Add files via upload

---
+++ CALCULADORA BASIC.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+    //VARIAVEIS
+    int A,B,X;
+           
+//ENTRADA DE VALORES
+printf("\n DIGITE O PRIMEIRO NÚMERO: \t");
+scanf("%d", &A);
+
+printf("\n DIGITE O SEGUNDO NÚMERO: \t");
+scanf("%d", &B);
+
+//SAÍDA DOS DADOS
+X= A+B;
+
+printf("\n O SEU NÚMERO É: \t");
+printf("%d",X);
+
+return 0;
+}
