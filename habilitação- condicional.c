aa21981f96cdcfb42be762b10dca5ca0a727b90d
walli778 <37522879+walli778@users.noreply.github.com>
2018-03-22 21:07:18 -0300

    Create habilitação- condicional.c

---
+++ habilitação- condicional.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main () {
+
+int idade;
+printf("\n\t DIGITE SUA IDADE: ");
+scanf("%d", &idade);
+
+if(idade>=18) {
+
+printf("APTO PARA HABILITAÇÃO"); }
+
+ else {
+    printf("VOCE AINDA NAO PODE DIRIGIR,");
+}
+
+}
+
